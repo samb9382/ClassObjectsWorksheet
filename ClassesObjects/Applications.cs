ea808a50693b27b5253ddf2928938994a21d655e
SammySupreme B <samb9382@gmail.com>
2020-05-18 09:23:37 -0500

    created an Applications class with member variables and a constructor

---
+++ ClassesObjects/Applications.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesObjects
+{
+    class Applications
+    {
+        //member variables
+        public string ApplicationName;
+        public string ApplicationType;
+        public double RequiredRAM;
+        public double RequiredStorage;
+
+        //constructor
+        public Applications()
+        {
+            ApplicationName = "Slack";
+            ApplicationType = "Messaging Service";
+            RequiredRAM = 4.0; //in gigabytes
+            RequiredStorage = 0.512; //in gigabytes
+        }
+    }
+}
